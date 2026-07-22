58f7c48f09c76a14963beedaced5c8c5cfba8a4e
Future209800 <pietromoni@hotmail.it>
2026-07-22 16:53:01 +0200

    Refactor build-webos.sh for module initialization
    Removed legacy module CMakeLists.txt creation and added source file initialization for StarfishMediaAPIs_C.cpp.

--- build-webos.sh
+++ build-webos.sh
@@ -555,9 +555,10 @@ set -x  # trace all commands from here so failures are visible
 rm -f "$BUILD_DIR/CMakeCache.txt"
 rm -rf "$BUILD_DIR/CMakeFiles"
 
-# Creiamo la cartella se non esiste e azzeriamo il file CMake del modulo legacy
-mkdir -p "$1/third-party/ss4s/modules/webos/smp"
-echo "" > "$1/third-party/ss4s/modules/webos/smp/CMakeLists.txt"
+# Azzeriamo il file sorgente che richiede l'header mancante
+mkdir -p "$1/third-party/ss4s/modules/webos/smp/wrapper"
+echo "" > "$1/third-party/ss4s/modules/webos/smp/wrapper/StarfishMediaAPIs_C.cpp"
+
 cmake -B "$BUILD_DIR" \
     -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" \
     -DCMAKE_BUILD_TYPE=Release \
